4784d5970a7cfeeac198b16680fa74015100a44d
canyan7n <canyann@163.com>
2023-04-07 22:19:35 +0800

    commit on idea -> precessing in springmvc 初识RESTFul

---
+++ spring-mvc-rest/src/main/java/com/canyan7n/controller/TestRestController.java
@@ -0,0 +1,15 @@
+package com.canyan7n.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :TestRestController
+ * @date ：2023/4/7 22:18
+ * @description：TODO
+ *
+ */
+@Controller
+public class TestRestController {
+}
