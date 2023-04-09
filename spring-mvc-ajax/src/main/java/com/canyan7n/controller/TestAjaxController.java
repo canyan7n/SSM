038cd28d63b036bf1e0eecd4be3a404d1c2b445a
canyan7n <canyann@163.com>
2023-04-09 23:24:22 +0800

    commit on idea -> precessing in springmvc 处理Ajax请求,向浏览器响应json格式数据

--- spring-mvc-ajax/src/main/java/com/canyan7n/controller/TestAjaxController.java
+++ spring-mvc-ajax/src/main/java/com/canyan7n/controller/TestAjaxController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,8 +29,21 @@ public class TestAjaxController {
     }
 
     @RequestMapping("/test/requestBody/json")
-    public void testRequestBody(@RequestBody User user,HttpServletResponse response,HttpServletRequest request) throws IOException {
+    public void testRequestBody(@RequestBody User user,HttpServletResponse response) throws IOException {
         System.out.println(user);
         response.getWriter().write("hello,json");
+    }
+
+    @RequestMapping("/test/responseBody")
+    @ResponseBody
+    public String testResponseBody(){
+        return "success";
+    }
+
+    @RequestMapping("/test/responseBody/json")
+    @ResponseBody
+    public User testResponseBodyJson(){
+        User user = new User(1002,"admins","123",22,"女");
+        return user;
     }
 }
