a5d230574e4e1b1e19478b20016b1121dad6d1ad
canyan7n <canyann@163.com>
2023-04-12 22:31:38 +0800

    commit on idea -> precessing in springmvc 初识SpringMVC 拦截器，FirstInterceptor

---
+++ spring-mvc-extension/src/main/java/com/canyan7n/interceptor/FirstInterceptor.java
@@ -0,0 +1,33 @@
+package com.canyan7n.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :FirstInterceptor
+ * @date ：2023/4/12 22:27
+ * @description：TODO
+ */
+public class FirstInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("FirstInterceptor-->preHandle");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("FirstInterceptor-->postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("FirstInterceptor-->afterCompletion");
+    }
+}
