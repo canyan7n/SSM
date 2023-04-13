e9e17d8e437200f69d7182183b22ce4ec7f2fcc9
canyan7n <canyann@163.com>
2023-04-13 17:11:50 +0800

    commit on idea -> precessing in springmvc 配置SpringMVC 拦截器执行顺序

---
+++ spring-mvc-extension/src/main/java/com/canyan7n/interceptor/SecondInterceptor.java
@@ -0,0 +1,35 @@
+package com.canyan7n.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :FirstInterceptor
+ * @date ：2023/4/12 22:27
+ * @description：TODO
+ */
+@Component
+public class SecondInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("SecondInterceptor-->preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("SecondInterceptor-->postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("SecondInterceptor-->afterCompletion");
+    }
+}
