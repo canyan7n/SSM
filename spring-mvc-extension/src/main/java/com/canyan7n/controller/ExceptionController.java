889495fa80490548a1a60b7335dca98407f9c804
canyan7n <canyann@163.com>
2023-04-14 14:50:33 +0800

    commit on idea -> precessing in springmvc 异常处理，注解方式完成

--- spring-mvc-extension/src/main/java/com/canyan7n/controller/ExceptionController.java
+++ spring-mvc-extension/src/main/java/com/canyan7n/controller/ExceptionController.java
@@ -14,13 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class ExceptionController {
-
+//    设置要处理的异常
     @ExceptionHandler(ArithmeticException.class)
+//    形参拿到异常信息
     public ModelAndView handleException(Throwable ex){
         ModelAndView modelAndView = new ModelAndView();
+//        向请求域共享数据
         modelAndView.addObject("ex",ex);
+//        跳转逻辑视图
         modelAndView.setViewName("error");
         return modelAndView;
     }
-
 }
