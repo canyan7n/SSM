f7e50ac520c79fc0dc387a626eb50cddf5405ca1
canyan7n <canyann@163.com>
2023-04-15 18:25:23 +0800

    commit on idea -> precessing in ssm整合,整合mybatis-01

---
+++ merge-ssm/src/main/java/com/canyan7n/mapper/EmpMapper.java
@@ -0,0 +1,11 @@
+package com.canyan7n.mapper;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :EmpMapper
+ * @date ：2023/4/15 17:55
+ * @description：TODO
+ */
+public interface EmpMapper {
+}
