fa69c83cd518d3eab6b7f072126d3e228c794321
canyan7n <canyann@163.com>
2023-04-15 17:52:30 +0800

    commit on idea -> precessing in ssm整合,配置spring.xml

---
+++ merge-ssm/src/main/java/com/canyan7n/service/impl/EmpServiceImpl.java
@@ -0,0 +1,15 @@
+package com.canyan7n.service.impl;
+
+import com.canyan7n.service.EmpService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :EmpServiceImpl
+ * @date ：2023/4/15 17:43
+ * @description：TODO
+ */
+@Service
+public class EmpServiceImpl implements EmpService {
+}
