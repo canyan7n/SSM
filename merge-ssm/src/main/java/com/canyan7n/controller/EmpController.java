282d4202f3f6acc1cfd72a69c67fab4f8eb79e5c
canyan7n <canyann@163.com>
2023-04-15 17:40:06 +0800

    commit on idea -> precessing in ssm整合,配置springMVC.xml

---
+++ merge-ssm/src/main/java/com/canyan7n/controller/EmpController.java
@@ -0,0 +1,15 @@
+package com.canyan7n.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :EmpController
+ * @date ：2023/4/15 17:38
+ * @description：TODO
+ */
+@Controller
+public class EmpController {
+}
